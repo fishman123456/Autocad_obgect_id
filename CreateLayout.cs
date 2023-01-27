02044307132e8ca0f3d7e44f33c5a0c6a4cc6689
denis <106389581+fishman123456@users.noreply.github.com>
2023-01-27 17:32:05 +0700

    добавил создание листов

---
+++ CreateLayout.cs
@@ -0,0 +1,62 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Geometry;
+
+namespace Autocad_obgect_id
+{
+  public class CreateLayout
+    {
+        int lay_counter = 0;
+        //27-01-2023 
+        // создание нового листа
+        // Create a new layout with the LayoutManager
+        [CommandMethod("F_CreateLayout")]
+    public void CreateLayout_Void()
+    {
+        // Get the current document and database
+        Document acDoc = Application.DocumentManager.MdiActiveDocument;
+        Database acCurDb = acDoc.Database;
+
+        // Get the layout and plot settings of the named pagesetup
+        using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+        {
+            // Reference the Layout Manager
+            LayoutManager acLayoutMgr = LayoutManager.Current;
+                // ввод имeни листа
+                string lay_Name;
+               
+                lay_counter++;
+               // acDoc.Editor.GetString(lay_Name);
+            // Create the new layout with default settings
+
+            ObjectId objID = acLayoutMgr.CreateLayout("newLayout" + lay_counter.ToString());
+
+            // Open the layout
+            Layout acLayout = acTrans.GetObject(objID,
+                                                OpenMode.ForRead) as Layout;
+
+            // Set the layout current if it is not already
+            if (acLayout.TabSelected == false)
+            {
+                acLayoutMgr.CurrentLayout = acLayout.LayoutName;
+            }
+
+            // Output some information related to the layout object
+            acDoc.Editor.WriteMessage("\nTab Order: " + acLayout.TabOrder +
+                                      "\nTab Selected: " + acLayout.TabSelected +
+                                      "\nBlock Table Record ID: " +
+                                      acLayout.BlockTableRecordId.ToString());
+
+            // Save the changes made
+            acTrans.Commit();
+        }
+    }
+}
+}
---
+++ ListPageSetup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.PlottingServices;
+namespace Autocad_obgect_id
+{
+
+    public class ListPageSetup
+    {
+        // пока неизвестная х-ня
+        // Lists the available page setups
+        [CommandMethod("F_ListPageSetup")]
+        public static void ListPageSetup_Void()
+        {
+            // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                DBDictionary plSettings = acTrans.GetObject(acCurDb.PlotSettingsDictionaryId,
+                                                            OpenMode.ForRead) as DBDictionary;
+
+                acDoc.Editor.WriteMessage("\nPage Setups: ");
+
+                // List each named page setup
+                foreach (DBDictionaryEntry item in plSettings)
+                {
+                    acDoc.Editor.WriteMessage("\n  " + item.Key);
+                }
+
+                // Abort the changes to the database
+               // acTrans.Abort();
+               acTrans.Commit();
+            }
+        }
+    }
+}
